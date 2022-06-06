9007b271f2bf6ad612b13476adb785bb44ac9ea6
izayoi1421 <mcdcnaguit@tip.edu.ph>
2022-06-07 00:12:02 +0800

    first commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3idos+=2q)q23&5kjwduz7dxunji!v^*majxlf7lv^#($2fdq8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mcdcnaguit-herokuapp.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['mcdcnaguit-herokuapp.herokuapp.com','127.0.0.1','*']
 
 
 # Application definition
