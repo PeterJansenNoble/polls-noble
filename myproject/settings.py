f4d531c22ff2e55e3e770fc0f118a60516b56789
izayoi1421 <mcdcnaguit@tip.edu.ph>
2022-06-06 23:50:52 +0800

    first commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3idos+=2q)q23&5kjwduz7dxunji!v^*majxlf7lv^#($2fdq8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mcdcnaguit-herokuapp.herokuapp.com','127.0.0.1']
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS= [os.path.join(BASE_DIR,"static")]
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
